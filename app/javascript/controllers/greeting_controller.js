7839435acdeaaa5dad375f7f57c9e422e982db22
Gilberto Oliveira Jr <giljr.2009@gmail.com>
2025-05-30 11:22:02 -0400

    Update greeting_controller.js

--- app/javascript/controllers/greeting_controller.js
+++ app/javascript/controllers/greeting_controller.js
@@ -4,7 +4,9 @@ export default class extends Controller {
   connect() {
     console.log("GreetingController connected");
   }
-
+  // Declares which elements your controller should recognize as "targets."
+  // Without this declaration, Stimulus doesn't know that data-<controller>-target="name" elements are linked to this controller, 
+  // so it does not create the this.nameTarget property.
   static targets = ["name", "output"];
 
   greet() {
